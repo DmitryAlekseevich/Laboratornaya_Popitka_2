6648cdf6b466933ed65d56125ec14ee50220bc14
DmitryAlekseevich <113106163+DmitryAlekseevich@users.noreply.github.com>
2022-09-25 13:07:12 +0300

    Консольный код программы
    Пойдет

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class IsLeapYear
+{
+    public static void Main()
+    {
+        for (int year = 1700; year <= 2022; year++)
+        {
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("{0} is a leap year.", year);
+                DateTime leapDay = new DateTime(year, 2, 29);
+                DateTime nextYear = leapDay.AddYears(1);
+                Console.WriteLine("   One year from {0} is {1}.",
+                                  leapDay.ToString("d"),
+                                  nextYear.ToString("d")); 
+            }
+        }
+    }
+}
